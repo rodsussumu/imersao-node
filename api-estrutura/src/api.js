b8b8cde6f9b30afae4f2a7e02c3c391d49efc432
Rodrigo Tanaka <rodsussumu@gmail.com>
2022-10-10 13:51:05 -0300

    add joi and api structure

--- api-estrutura/src/api.js
+++ api-estrutura/src/api.js
@@ -1,8 +1,9 @@
-const Hapi = require('hapi')
+const Hapi = require('@hapi/hapi')
 const ContextStrategy = require('./db/strategies/base/contextStrategy')
 const MongoDb = require('./db/strategies/mongodb/mongodb')
 const HeroiSchema = require('./db/strategies/mongodb/schemas/heroisSchema')
 const HeroRoutes = require('./routes/heroRoutes')
+const Joi = require('joi')
 
 const app = new Hapi.Server({
     port: 5000,
@@ -15,6 +16,7 @@ function mapRoutes(instance, methods) {
 async function main() {
     const connection = MongoDb.connect()
     const context = new ContextStrategy(new MongoDb(connection, HeroiSchema))
+    app.validator(Joi)
     app.route([
       ...mapRoutes(new HeroRoutes(context), HeroRoutes.methods())  
     ])
